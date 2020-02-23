ff12920bb4592213ef77d6661e902db9d2b68012
wupeng <726451064@qq.com>
2020-02-23 21:26:42 +0800

    [add]结算和订单完善

---
+++ gmall-order-service/src/test/java/com/wp/gmall/order/GmallOrderServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.wp.gmall.order;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runner.Runner;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@RunWith(Runner.class)
+class GmallOrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
