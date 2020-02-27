59a139a3b0ecb90783dc1ae6758de26342204b47
wupeng <726451064@qq.com>
2020-02-27 17:48:50 +0800

    [add]秒杀

---
+++ gmall-seckill/src/main/java/com/wp/gmall/seckill/controller/SeckillController.java
@@ -0,0 +1,18 @@
+/**
+ * FileName: SeckillController
+ * Author: WP
+ * Date: 2020/2/27 16:12
+ * Description:
+ * History:
+ **/
+package com.wp.gmall.seckill.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@Controller
+@CrossOrigin
+public class SeckillController {
+
+    
+}
